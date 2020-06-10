1293b6c99523cb2997de99cb596718d4bea035bc
gpiras <gpiras@mac.com>
2020-06-10 11:48:47 -0400

    share the data

---
+++ R/CodeProbit.R
@@ -0,0 +1,100 @@
+#######################################################
+# Lab: Binary Choice Model
+# By: Mauricio Sarrias
+# Crated on: Sept 15, 2016
+# Warning: Not completely check
+#######################################################
+
+
+
+# I. Load the data base ====
+
+library("foreign")
+setwd("/home/gpiras/R dev lib/Logit Family/")
+binlfp2 <- read.dta("binlfp2.dta", convert.factors = FALSE)
+names(binlfp2)
+
+
+# II. Create model frame using Formula package ====
+library("Formula")
+F1  <- Formula(lfp ~ k5 + k618 + age + wc + hc)
+mf1 <- model.frame(F1, data = binlfp2)
+X <- model.matrix(F1, data = binlfp2, rhs = 1)
+y <- model.response(mf1)
+
+# III. Function for probit ====
+Probit.ml <- function(par, y, X, type = c("logit", "probit")) {
+    #Compute LL
+    if (type == "logit") {
+      F <- plogis
+      f <- dlogis
+      ff <- function(x) plogis(x) * (1 - plogis(x)) * (1 - 2 * plogis(x))
+    } else {
+      F <- pnorm
+      f <- dnorm
+      ff <- function(x) -x * dnorm(x)
+    }
+
+    par <- as.matrix(par)
+    qi <- 2 * y - 1
+    xb <- crossprod(t(X), par)
+    ll <- log(F(qi * xb))
+
+    #Compute gradient
+    lambdai <- qi * (f(qi * xb) / F(qi * xb))
+    attr(ll, "gradient") <- drop(lambdai) * X
+
+    #Hessian
+    hi <- drop(((ff(qi * xb) / F(qi * xb)) - ((f(qi * xb) / F(qi * xb))) ^ 2)) * qi ^ 2 * X
+    H <- crossprod(hi, X)
+    attr(ll, 'hessian') <- H
+    ll
+  }
+
+
+# IV. Function for probit ====
+par <- lm(y ~ X - 1)$coeff  #starting values
+library("maxLik")
+
+## BHHH Method
+t1 <- proc.time()
+Est1 <- maxLik(logLik = Probit.ml, start = par, y = y, X = X, method = "BHHH")
+print(round((proc.time() - t1)[3]/60,9))
+
+#"Newton-Raphson" Method
+t1 <- proc.time()
+Est2 <- maxLik(logLik = Probit.ml, start = par, y = y, X = X, method = "Newton-Raphson", type = "logit")
+print(round((proc.time() - t1)[3]/60,9))
+
+#"BFGS" Method
+t1 <- proc.time()
+Est3 <- maxLik(logLik = Probit.ml, start = par,y = y, X = X, method = "BFGS")
+print(round((proc.time() - t1)[3]/60,9))
+
+
+attributes(Est1)
+Est1$estimate
+Est2$estimate
+Est3$estimate
+
+  glm(y ~X-1, family = binomial(link = "probit"))
+## Get standard errors
+
+G <- Est1$gradientObs
+V <- 0
+for (i in 1:nrow(X)) {
+  V <- V + crossprod(t(G[i,]))
+}
+Var1 <- solve(V)
+sd1  <- sqrt(diag(Var1))
+
+H <- Est2$hessian
+Var2 <- -solve(H)
+sd2 <- sqrt(diag(Var2))
+
+H2   <- Est3$hessian
+Var3 <- -solve(H2)
+sd3  <- sqrt(diag(Var3))
+
+Table <- cbind(Est1$estimate, sd1, sd2, sd3)
+round(Table,7)
--- R/logit.R
+++ R/logit.R
@@ -2,7 +2,8 @@ rm(list=ls())
 library(haven)
 library(boot)
 library(Matrix)
-MROZ <- read_dta("~/Downloads/MROZ.DTA")
+
+MROZ <- read_dta("~/Downloads/MROZ.dta")
 
 y <- MROZ$inlf
 x <- cbind(1,MROZ$nwifeinc,MROZ$educ,MROZ$exper,MROZ$expersq,  MROZ$age, MROZ$kidslt6,MROZ$kidsge6)
@@ -11,14 +12,14 @@ x <- cbind(1,MROZ$nwifeinc,MROZ$educ,MROZ$exper,MROZ$expersq,  MROZ$age, MROZ$ki
 ###LOGIT MODEL#####
 ###################
 
-n0 = 1
-z <- log(( y + 0.5)/(n0 - y + 0.5))
-beta0 <- coefficients(lm(z ~x-1))
-betan <- rep(0.1,ncol(x))
-i = 1
-max.it = 1
+# n0 = 1
+# z <- log(( y + 0.5)/(n0 - y + 0.5))
+# beta0 <- coefficients(lm(z ~x-1))
+# betan <- rep(0.1,ncol(x))
+#i = 1
+#max.it = 1
 #IWLS
-while(sum(abs(beta0-betan)) >= 1e-8 || max.it == 500){
+#while(sum(abs(beta0-betan)) >= 1e-8 || max.it == 500){
 
   if(i==1) eta <- x %*% beta0
   else eta <- x %*% betan
@@ -35,16 +36,16 @@ print(ll)
 i = i +1
 }
 
-logit.17.1 <- glm(inlf ~ nwifeinc + educ + exper + expersq + age +
-                    kidslt6 + kidsge6,
-                  data = MROZ,
-                  family = binomial(link = "logit"))
+# logit.17.1 <- glm(inlf ~ nwifeinc + educ + exper + expersq + age +
+#                     kidslt6 + kidsge6,
+#                   data = MROZ,
+#                   family = binomial(link = "logit"))
 
 #log.likelihood
 loglik.logit <- function(par, y = NULL,x){
-  ll = -sum(-log(1+exp(x%*%par))) - sum(y* x%*%par)  
+  ll = -sum(-log(1+exp(x%*%par))) - sum(y* x%*%par)
   #print(ll)
-} 
+}
 opt <- nlminb(coefficients(lm(y~as.matrix(x)-1)),loglik.logit, y = y, x = as.matrix(x))
 opt$objective
 opt$par
@@ -55,39 +56,16 @@ opt
 ############################
 
 
-n0 = 1
-z <- log(( y + 0.5)/(n0 - y + 0.5))
-beta0 <- coefficients(lm(z ~x-1))
-betan <- rep(0.1,ncol(x))
-i = 1
-max.it = 1
 
-while(sum(abs(beta0-betan)) >= 1e-8 || max.it == 500){
-  
-  if(i==1) eta <- x %*% beta0
-  else eta <- x %*% betan
-  beta0 <- betan
-  muh <- inv.probit(as.numeric(eta) )
-  zs <- eta + (y - muh)/(muh*(n0-muh)) *n0
-  wi <- muh *(n0 - muh)/n0
-  W <- Diagonal(nrow(x))
-  diag(W) <- wi
-  betan <- solve(crossprod(x,W%*%x)) %*% crossprod(x, W%*%zs)
-  ll = -sum(-log(1+exp(x%*%betan))) - sum(y* x%*%betan)
-  #ll = (sum(y*  log(exp(x %*% betan)/( 1+ exp(x %*% betan))) + sum((1-y)*log(1-(exp(x %*% betan)/( 1+ exp(x %*% betan)))))))
-  print(ll)
-  i = i +1
-}
-
-probit.17.1 <- glm(inlf ~ nwifeinc + educ + exper + expersq + age +
-                    kidslt6 + kidsge6,
-                  data = MROZ,
-                  family = binomial(link = "probit"))
+# probit.17.1 <- glm(inlf ~ nwifeinc + educ + exper + expersq + age +
+#                     kidslt6 + kidsge6,
+#                   data = MROZ,
+#                   family = binomial(link = "probit"))
 
 loglik.probit <- function(par, y = NULL,x){
   ll = -sum(log(pnorm((2*y-1)*x%*%par)))
   print(ll)
-} 
-opt <- nlminb(coefficients(lm(y~as.matrix(x)-1)),loglik.probit, y = y, x = as.matrix(x))
+}
+opt <- nlminb(coefficients(lm(y~X-1)),loglik.probit, y = y, x = X)
 opt$objective
 opt$par
